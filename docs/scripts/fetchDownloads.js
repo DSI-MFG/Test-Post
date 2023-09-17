66d79da2420ef6b74c9d748bdf491f9b56b3ca7e
DrPhilDSI <109159255+DrPhilDSI@users.noreply.github.com>
2023-09-17 01:47:56 -0700

    test

--- docs/scripts/fetchDownloads.js
+++ docs/scripts/fetchDownloads.js
@@ -1,30 +1,56 @@
 const owner = "DSI-MFG";
 const repo = "Test-Post";
-const folderPath = "main/docs"; // Adjust accordingly
+const folderPath = "docs"; // Adjust accordingly
 
-console.log("Fetching repository contents...");
-
-fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${folderPath}`)
-  .then((response) => {
+async function fetchDownloads(owner, repo, folderPath) {
+  try {
+    const response = await fetch(
+      `https://api.github.com/repos/${owner}/${repo}/contents/${folderPath}`
+    );
     if (!response.ok) {
-      throw new Error(
-        `Error fetching repository contents: ${response.statusText}`
-      );
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
-    return response.json();
-  })
-  .then((data) => {
-    if (!Array.isArray(data)) {
-      throw new Error("Expected data to be an array of repository contents");
-    }
-    const cpsFiles = data.filter((file) => file.name.endsWith(".cps"));
+    const data = await response.json();
+    const cpsFiles = data.filter((file) => file.path.endsWith(".cps"));
+
     cpsFiles.forEach((file) => {
-      console.log(file.name);
+      // Fetch the file content and create a download link
+      fetch(file.download_url)
+        .then((response) => response.blob())
+        .then((blob) => {
+          const downloadsDiv = document.querySelector(".downloads");
+          const objectURL = URL.createObjectURL(blob);
+          const link = document.createElement("a");
+          link.href = objectURL;
+          link.download = file.name; // Suggests the name for the download
+          link.innerText = `Download ${file.name}`;
+          link.classList.add("download-link");
+          downloadsDiv.appendChild(link);
+        });
     });
-  })
-  .catch((error) => {
+  } catch (error) {
+    console.error("There was a problem with the fetch operation:", error);
+  }
+}
+
+async function fetchReadme(owner, repo, folderPath) {
+  try {
+    const response = await fetch(
+      `https://api.github.com/repos/${owner}/${repo}/contents/${folderPath}/README.md`
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    const decodedReadme = atob(data.content);
+
+    const readmeDiv = document.querySelector(".readme-section");
+    readmeDiv.innerHTML = `<h2>README</h2><pre>${decodedReadme}</pre>`;
+  } catch (error) {
     console.error(
       "There was a problem with the fetch operation:",
       error.message
     );
-  });
+  }
+}
--- docs/scripts/login.js
+++ docs/scripts/login.js
@@ -4,6 +4,8 @@ function check(form) {
   if (form.userid.value == username && form.pswrd.value == password) {
     document.querySelector(".login-card").style.display = "none";
     document.querySelector(".content-card").style.display = "block";
+    fetchDownloads("DSI-MFG", "Test-Post", "docs");
+    fetchReadme("DSI-MFG", "Test-Post", "docs");
   } else if (form.userid.value == username) {
     alert("Incorrect Password");
   } else {
